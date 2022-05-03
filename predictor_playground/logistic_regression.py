c09dce29f4e17588ce90d52910aa01d6566af58b
Nate Buel <natebuel29@gmail.com>
2022-05-02 21:40:12 -0500

    got gradient descent working, but am struggling with the optimize.minimize functionality

--- predictor_playground/logistic_regression.py
+++ predictor_playground/logistic_regression.py
@@ -10,6 +10,7 @@ class LogisticRegression:
         # X_tr[:,i] = (X_tr[:,i] - np.mean(X_tr[:,i]))/np.std(X_tr[:,i])
         # https://medium.com/@martinpella/logistic-regression-from-scratch-in-python-124c5636b8ac
         # https://github.com/drbilo/logistic-regression
+        #
         self.X = X
         self.y = y
         self.theta = theta
@@ -21,9 +22,15 @@ class LogisticRegression:
         return self.sigmoid(self.X.dot(np.transpose(self.theta)))
 
     def cost(self):
-        # this is working but would like to test it more
         m = self.y.size
         h = self.h_of_x()
-        first_part = (-self.y).dot(np.log(h))
-        print(first_part)
         return (1/m)*((-self.y).dot(np.log(h)) - (1-self.y).dot(np.log(1-h)))
+
+    def gradient_descent(self, alpha, iters):
+        m = self.y.size
+        j_history = []
+        for i in range(0, iters):
+            h = self.h_of_x()
+            self.theta = self.theta - (alpha/m)*((h-self.y).dot(self.X))
+            j_history.append(self.cost())
+        return j_history
--- tests/test_logistic_regression.py
+++ tests/test_logistic_regression.py
@@ -1,6 +1,9 @@
 import numpy as np
 from predictor_playground.logistic_regression import LogisticRegression
 import unittest
+from scipy.optimize import minimize, fmin_tnc
+import csv
+import matplotlib.pyplot as plt
 
 
 class TestLogisticRegression(unittest.TestCase):
@@ -22,11 +25,53 @@ class TestLogisticRegression(unittest.TestCase):
         self.assertEqual(np.around(results, decimals=3).all(), expected.all())
 
     def test_cost(self):
-        # finish this test
         theta = np.array([0.2, 0.1, 0.5])
         X = np.array([[1, 1, 1], [1, 1, 1]])
         y = np.array([1.0, 0.0])
-        expected = 0
+        expected = .7711
         logReg = LogisticRegression(X, y, theta)
         results = logReg.cost()
-        self.assertEqual(results, expected)
+        J, gradient = costFunction(theta, X, y)
+        self.assertEqual(np.around(results, decimals=4), expected)
+
+    def test_gradient_descent(self):
+        irisdata = np.loadtxt(
+            "C:\\Users\\nateb\\Documents\\Repos\\python\\ufc-stats-predictor-playground\\tests\\irisdata.csv", delimiter=",", skiprows=1).astype("float")
+        rows, columns = irisdata.shape
+        theta = np.array([0, 0, 0])
+        y = irisdata[:, columns-1]
+        X = np.concatenate([np.ones((rows, 1)),
+                            irisdata[:, 0:columns-1]], axis=1)
+        logReg = LogisticRegression(X, y, theta)
+        iteratations = 30000
+        j_history = logReg.gradient_descent(0.01, iteratations)
+        res = minimize(
+            costFunction,  np.array([0, 0, 0]), (X, y), jac=True, method='TNC', options={'maxiter': 400})
+        print(res.x)
+        print(res.fun)
+        # print(res.x)
+        # plotChart(iteratations, j_history)
+
+        # (logReg.theta)
+
+
+def costFunction(theta, X, y):
+    def sigmoid(h):
+        return (1.0/(1.0+np.exp(-h)))
+    m = y.size
+    h = sigmoid(X.dot(np.transpose(theta)))
+    print(h)
+    J = (1/m)*((-y).dot(np.log(h+.0000001)) - (1-y).dot(np.log(1-h+.0000001)))
+    grad = ((h-y).dot(X))/m
+
+    return J, grad
+
+
+def plotChart(iterations, cost_num):
+    fig, ax = plt.subplots()
+    ax.plot(np.arange(iterations), cost_num, 'r')
+    ax.set_xlabel('Iterations')
+    ax.set_ylabel('Cost')
+    ax.set_title('Error vs Iterations')
+    plt.style.use('fivethirtyeight')
+    plt.show()
