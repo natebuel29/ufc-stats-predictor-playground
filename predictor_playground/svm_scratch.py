9c6891ca825cc3864cd48085df7ec8b2620de49a
Nate Buel <natebuel29@gmail.com>
2022-08-02 22:08:20 -0500

    svm impl watching a vid

--- predictor_playground/svm_scratch.py
+++ predictor_playground/svm_scratch.py
@@ -13,16 +13,140 @@ class Support_Vector_Machine:
             self.ax = self.fig.add_subplot(1, 1, 1)
 
     def fit(self, data):
-        pass
+        self.data = data
+        # { | | w | | : [w,b] }
+        opt_dict = {}
+
+        transforms = [[1, 1],
+                      [-1, 1],
+                      [-1, -1],
+                      [1, -1]]
+
+        all_data = []
+
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+
+        del all_data
+
+        step_sizes = [self.max_feature_value * 0.1,
+                      self.max_feature_value * 0.01,
+                      # point of expense:
+                      self.max_feature_value * 0.001]
+        # extremely expensive
+        b_range_multiple = 5
+        # we don't need to take as small of a step aith b as we do w
+        b_multiple = 5
+
+        latest_optimim = self.max_feature_value * 10
+
+        for step in step_sizes:
+            w = np.array([latest_optimim, latest_optimim])
+            # we can do this because convex
+            optimized = False
+
+            while not optimized:
+                for b in np.arange(-1*(self.max_feature_value*b_range_multiple), self.max_feature_value*b_range_multiple, step*b_multiple):
+                    for tranformation in transforms:
+                        w_t = w*tranformation
+                        found_option = True
+                        # weakest link in the svm fundamentally
+                        # SMO attempts to fix this a bit
+                        # yi*(xi.w+b)>=1
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi*(np.dot(w_t, xi)+b) >= 1:
+                                    found_option = False
+                                #print(yi*(np.dot(w_t, xi)+b))
+
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+
+                if w[0] < 0:
+                    optimized = True
+                    print('Optimized a step')
+                else:
+                    w = w - step
+
+            norms = sorted([n for n in opt_dict])
+
+            opt_choice = opt_dict[norms[0]]
+
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimim = opt_choice[0][0] + +step*2
 
     def predict(self, features):
         #sign(x.w + b)
         classification = np.sign(np.dot(np.array(features), self.w)+self.b)
 
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200,
+                            marker='*', c=self.colors[classification])
 
-data_dict = {-1: np.array([1, 7],
+        return classification
+
+    def visualize(self):
+        [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i])
+          for x in data_dict[i]] for i in data_dict]
+        #hyperplane = x.w+b
+        #v = x.w+b
+        #psv = 1
+        #nsv = -1
+        #dec = 0
+
+        def hyperplane(x, w, b, v):
+            return(-w[0]*x-b+v)/w[1]
+
+        datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        # positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2])
+
+        # (w.x+b) = -1
+        # negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2])
+
+        # (w.x+b) = 0
+        # positive support vector hyperplane
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2])
+
+        plt.show()
+
+
+data_dict = {-1: np.array([[1, 7],
                           [2, 8],
-                          [3, 8]),
-             1: np.array([5, 1],
+                          [3, 8]]),
+             1: np.array([[5, 1],
                          [6, -1],
-                         [7, 3])}
+                         [7, 3]])}
+
+svm = Support_Vector_Machine()
+
+svm.fit(data=data_dict)
+
+predict_us = [[0, 10], [1, 3], [4, 5], [6, -4], [5, 8]]
+
+for p in predict_us:
+    svm.predict(p)
+
+svm.visualize()
